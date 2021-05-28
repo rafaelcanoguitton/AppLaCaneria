089c5b9578b701e9ba898bf463e6b00c8aa022ae
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-05-28 15:04:23 -0500

    "Updated carrito"

--- lib/carrito.dart
+++ lib/carrito.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:path_provider/path_provider.dart';
+
 class CarritoCompras extends StatefulWidget {
   List<TripleWea> itemsitos;
   final Function handlersito;
@@ -22,58 +23,77 @@ class CarritoComprasState extends State<CarritoCompras> {
       otro_handler(itemsitos);
     });
   }
+
   String _baseUrl = "https://api-db--pizzeria-la-caneria.herokuapp.com/";
-  void ordenar_api_handler()async {
-    List<Item> ask=[];
-    for(var i=0;i<itemsitos.length;i++){
-      ask.add(Item(nombre: itemsitos[i].nombre,precio: itemsitos[i].precio,tamao: itemsitos[i].desc));
+  void ordenar_api_handler() async {
+    List<Item> ask = [];
+    for (var i = 0; i < itemsitos.length; i++) {
+      ask.add(Item(
+          nombre: itemsitos[i].nombre,
+          precio: itemsitos[i].precio,
+          tamao: itemsitos[i].desc));
     }
-    var itemes=Menu(
-        medioDePago: "efectivo",
-        ubicacion: "prueba",
-        comentario: " ",
-        items: ask,
+    var itemes = Menu(
+      medioDePago: "efectivo",
+      ubicacion: "prueba",
+      comentario: " ",
+      items: ask,
     );
     try {
-      var teste=menuToJson(itemes);
+      var teste = menuToJson(itemes);
       var appDocDir = await getApplicationDocumentsDirectory();
       String appDocPath = appDocDir.path;
       var dio = Dio();
-      dio.interceptors.add(CookieManager(PersistCookieJar(ignoreExpires: true,storage: FileStorage(appDocPath +"/.cookies/"))));
+      dio.interceptors.add(CookieManager(PersistCookieJar(
+          ignoreExpires: true,
+          storage: FileStorage(appDocPath + "/.cookies/"))));
       final response = await dio.post('${_baseUrl}hacer_pedido', data: teste);
       showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-                  title: Text('Aviso'),
-                  content: Text(response.data),
-                  actions: [
-                    TextButton(onPressed: () {Navigator.of(context).pop();Navigator.of(context).pop();}, child: Text('Ok')),
-                    //TextButton(onPressed: () {}, child: Text('Cancel'))
-                  ],
-                ));
-      List<TripleWea> replace=[];
-      itemsitos=replace;
-      otro_handler( itemsitos);
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('Aviso'),
+                content: Text(response.data),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Ok')),
+                  //TextButton(onPressed: () {}, child: Text('Cancel'))
+                ],
+              ));
+      List<TripleWea> replace = [];
+      itemsitos = replace;
+      otro_handler(itemsitos);
     } catch (e) {
-      if(e.type==DioErrorType.other)
+      if (e.type == DioErrorType.other)
         showDialog(
             context: context,
             builder: (context) => AlertDialog(
                   title: Text('Aviso'),
                   content: Text('Ocurrió un error en el servidor'),
                   actions: [
-                    TextButton(onPressed: () {Navigator.of(context).pop();}, child: Text('Ok')),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Ok')),
                     //TextButton(onPressed: () {}, child: Text('Cancel'))
                   ],
                 ));
-      else{
+      else {
         showDialog(
             context: context,
             builder: (context) => AlertDialog(
                   title: Text('Aviso'),
                   content: Text(e.response.data),
                   actions: [
-                    TextButton(onPressed: () {Navigator.of(context).pop();}, child: Text('Ok')),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Ok')),
                     //TextButton(onPressed: () {}, child: Text('Cancel'))
                   ],
                 ));
@@ -85,34 +105,94 @@ class CarritoComprasState extends State<CarritoCompras> {
     // print("response.statusCode");
     // print(response.statusCode);
   }
+
   List<TripleWea> itemsitos;
   final Function otro_handler;
   CarritoComprasState(this.itemsitos, this.otro_handler);
   List<Widget> construc() {
     List<Widget> a = [];
-    for (var i = 0; i < itemsitos.length; i++) {
-      a.add(Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(itemsitos[i].nombre),
-          Text('S/.' + itemsitos[i].precio.toString()),
-          Text(itemsitos[i].desc),
-          IconButton(
-            icon: Icon(
-              Icons.delete_outline,
-              size: 20.0,
-              color: Colors.red[900],
+    a.add(
+      Container(
+        height: 90,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_back_outlined,),
             ),
-            onPressed: () {
-              delete_handler(i);
-            },
-          )
-        ],
+            Expanded(
+              child: Text(
+                "Tu pedido",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+              ),
+            ),
+            Container(child: Text("           "),),
+          ],
+        ),
+      ),
+    );
+    for (var i = 0; i < itemsitos.length; i++) {
+      a.add(Container(
+        decoration: BoxDecoration(
+          border: Border.symmetric(
+            horizontal: BorderSide(color: Colors.black54),
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10),
+        child: Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              width: 150,
+              height: 90,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.transparent),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/fondito.jpg"),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  itemsitos[i].nombre,maxLines: 2,
+                  
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'S/.' + itemsitos[i].precio.toString(),
+                ),
+                Text(itemsitos[i].desc),
+                IconButton(
+                  icon: Icon(
+                    Icons.delete_outline,
+                    size: 20.0,
+                    color: Colors.red[900],
+                  ),
+                  onPressed: () {
+                    delete_handler(i);
+                  },
+                )
+              ],
+            ),
+          ],
+        ),
       ));
     }
     return a;
   }
 
+// Text(itemsitos[i].nombre),
+//           Text('S/.' + itemsitos[i].precio.toString()),
+//           Text(itemsitos[i].desc),
   @override
   Widget build(BuildContext context) {
     List<Widget> compras = construc();
@@ -133,7 +213,9 @@ class CarritoComprasState extends State<CarritoCompras> {
               children: [
                 ElevatedButton(
                   child: Text('Ordenar'),
-                  onPressed: (){ordenar_api_handler();},
+                  onPressed: () {
+                    ordenar_api_handler();
+                  },
                 ),
               ],
             ))
